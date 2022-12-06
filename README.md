ab27da560f6f29f78b6ea53da4fb9085f079cf72
HerbertDuarte <herbertduarte.santos@gmail.com>
2022-12-05 23:45:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Esse repositório é referente a um projeto de slider horizontal (carrossel) com HTML, CSS e JS(puro).
---
+++ script.js
@@ -0,0 +1,16 @@
+let i = 1
+document.getElementById("radio1").checked = true
+    
+ setInterval(function(){
+     nextImage()
+},4000)
+
+ function nextImage(){
+     i++
+
+     if (i > 5) {
+         i = 1
+   }
+
+document.getElementById(`radio${i}`).checked = true
+}
